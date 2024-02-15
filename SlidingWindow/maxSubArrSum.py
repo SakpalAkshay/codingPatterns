cb2c2795b907914947641907fccea17be4db51ef
Akshay Sakpal <131487996+SakpalAkshay@users.noreply.github.com>
2024-02-14 17:41:50 -0800

    pushed code

--- SlidingWindow/maxSubArrSum.py
+++ SlidingWindow/maxSubArrSum.py
@@ -1,1 +1,27 @@
+#Let's say we have an array [1, 3, 5, 2, 7, 4] and we want to find the maximum sum of a subarray of size 3.
 
+
+arr = [1,3,5,2,7,4]
+k = 3
+
+hashSet ={}
+def maxSum(arr,k):
+    max = 0
+    
+    for i in range(len(arr)):
+        sum = 0
+        subArr = []
+        for j in range(i,min(len(arr),i+k)):
+            sum+= arr[j]
+            subArr.append(arr[j])
+        hashSet[sum] = subArr
+        print(hashSet)
+        if sum > max:
+            max = sum
+        print(max)
+    if max in hashSet:
+        return max, hashSet[max]
+    else:
+        return False
+maxVal, subarr = maxSum(arr, k)
+print(maxVal, subarr)
